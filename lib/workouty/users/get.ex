684d2135fa07b480c3a92c4dbb4252128c462a2d
regisfaria <rgisfaria@yahoo.com.br>
2021-06-24 20:47:14 -0300

    user is now coming with trainings and exercices

--- lib/workouty/users/get.ex
+++ lib/workouty/users/get.ex
@@ -1,6 +1,8 @@
 defmodule Workouty.User.Get do
+  import Ecto.Query
+
   alias Ecto.UUID
-  alias Workouty.{Repo, User}
+  alias Workouty.{Repo, User, Training}
 
   def call(id) do
     id
@@ -15,7 +17,16 @@ defmodule Workouty.User.Get do
   defp handle_response({:ok, uuid}) do
     case Repo.get(User, uuid) do
       nil -> {:error, "Invalid UUID"}
-      user -> {:ok, user}
+      user -> {:ok, load_training(user)}
     end
+  end
+
+  defp load_training(user) do
+    today = Date.utc_today()
+
+    query = from training in Training,
+     where: ^today >= training.start_date and ^today <= training.end_date
+
+    Repo.preload(user, trainings: {first(query, :inserted_at), :exercises})
   end
 end
